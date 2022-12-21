ce1a65aff61072d1ef9f21c0f21202642263d5bf
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-12-21 12:13:27 +0300

    Karar ve Dongu Yapıları

--- app/src/main/java/com/example/unite4_kararvedonguyapilari/MainActivity.java
+++ app/src/main/java/com/example/unite4_kararvedonguyapilari/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.unite4_kararvedonguyapilari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btnUyg1Goster(View view){
+        Intent i = new Intent(MainActivity.this,uyg1Activitiy.class);
+        startActivity(i);
+    }
+    public void btnUyg2Goster(View view){
+        Intent i = new Intent(MainActivity.this,uyg2Activity.class);
+        startActivity(i);
     }
 }
